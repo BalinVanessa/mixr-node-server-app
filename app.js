1a374e7883fedb68dd371c2b3151e370273e5d0a
Jack Rifkin <jack.rifkin@icloud.com>
2023-12-01 18:32:29 -0500

    users mongoDB API

---
+++ app.js
@@ -0,0 +1,36 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import UserRoutes from "./users/routes.js";
+import session from "express-session";
+mongoose.connect("mongodb://127.0.0.1:27017/mixr");
+
+const app = express();
+
+app.use(
+    cors({
+        credentials: true,
+        origin: process.env.FRONTEND_URL
+    })
+);
+
+
+const sessionOptions = {
+    secret: "any string",
+    resave: false,
+    saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+        sameSite: "none",
+        secure: true,
+    };
+}
+app.use(session(sessionOptions));
+
+app.use(express.json());
+
+UserRoutes(app);
+
+app.listen(4000);
