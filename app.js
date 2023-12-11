5e87956d44648ab35158cc0f15539b81016a21af
BalinVanessa <vanashbalin@gmail.com>
2023-12-10 19:06:37 -0500

    Fixed imports in app.js file

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ import cors from "cors";
 import mongoose from "mongoose";
 import UserRoutes from "./users/routes.js";
 import session from "express-session";
+import DrinksRoutes from "./drinks/routes.js";
+import FavoritesRoutes from "./favorites/routes.js";
+import FollowsRoutes from "./follows/routes.js";
+import IngredientRoutes from "./ingredients/routes.js";
 
 const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/mixr';
 mongoose.connect(CONNECTION_STRING);
@@ -34,5 +38,9 @@ app.use(session(sessionOptions));
 app.use(express.json());
 
 UserRoutes(app);
+DrinksRoutes(app);
+FavoritesRoutes(app);
+FollowsRoutes(app);
+IngredientRoutes(app);
 
 app.listen(4000);
