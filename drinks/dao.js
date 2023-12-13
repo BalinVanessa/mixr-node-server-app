2bb3455dea8ead3f70a4b6adb1931594d7c3b579
Jack Rifkin <jack.rifkin@icloud.com>
2023-12-13 18:13:42 -0500

    add findDrinksByName

--- drinks/dao.js
+++ drinks/dao.js
@@ -9,6 +9,12 @@ export const findDrinkById = (idDrink) => model.findOne({idDrink : idDrink});
 export const findOneDrinkByName = (drinkName) =>
     model.findOne({ strDrink: drinkName });
 
+export const findAllDrinksByName = (drinkName) => {
+    const regex = new RegExp(`^${drinkName}`, 'i');
+    const drinks = model.find({strDrink: regex});
+    return drinks;
+}
+
 export const updateDrink = (idDrink, drink) =>
     model.updateOne({ idDrink: idDrink}, { $set: drink });
 
