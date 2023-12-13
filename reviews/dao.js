cfc87991d89c735d6c4c8928447ddb86115b6233
BalinVanessa <vanashbalin@gmail.com>
2023-12-13 14:53:03 -0500

    Added date field to Reviews schema. Added routes for Review

--- reviews/dao.js
+++ reviews/dao.js
@@ -1,17 +1,20 @@
 import model from "./model.js";
 
-export const findAllReviews = () => model.find();
+export const findAllReviews = () => model.find().sort({date: -1});
 
-export const createUserReviewDrink = (review) =>
+export const findReviewById = (reviewId) =>
+    model.findById(reviewId);
+
+export const createReview = (review) =>
     model.create(review);
 
-export const deleteUserReviewDrink = (reviewId) =>
+export const deleteReview = (reviewId) =>
     model.deleteOne({ _id: reviewId });
 
 export const findReviewsForDrink = (drinkId) =>
-    model.find({ idDrink: drinkId });
+    model.find({ idDrink: drinkId }).sort({date: -1});
 
-export const findReviewsFromUser = (userId) => model.find({ user: userId });
+export const findReviewsFromUser = (userId) => model.find({ user: userId }).sort({date: -1});
 
-export const editReview = (idDrink, review) =>
-    model.updateOne({ idDrink: idDrink }, { $set: review });
+export const editReview = (reviewId, review) =>
+    model.updateOne({ _id: reviewId }, { $set: review });
---
+++ reviews/routes.js
@@ -0,0 +1,54 @@
+import * as dao from "./dao.js";
+
+function ReviewsRoutes(app) {
+    const findAllReviews = async (req, res) => {
+        const reviews = await dao.findAllReviews();
+        res.json(reviews);
+    };
+
+    const createReview = async (req, res) => {
+        const review = await dao.createUserReviewDrink(req.body);
+        res.json(review);
+    };
+
+    const deleteReview = async (req, res) => {
+        const status = await dao.deleteUserReviewDrink(req.params.reviewId);
+        res.json(status);
+    };
+
+    const findReviewsForDrink = async (req, res) => {
+        const idDrink = req.params.idDrink;
+
+        const reviews = await dao.findReviewsForDrink(idDrink);
+        res.json(reviews);
+    };
+
+    const findReviewsFromUser = async (req, res) => {
+        const userId = req.params.userId;
+        const reviews = await dao.findReviewsFromUser(userId);
+        res.json(reviews);
+    };
+
+    const findReviewById = async (req, res) => {
+        const { reviewId } = req.params;
+        const review = await dao.findReviewById(reviewId);
+        res.json(review);
+    }
+
+    const editReview = async (req, res) => {
+        const { reviewId } = req.params;
+        const status = await dao.editReview(reviewId, req.body);
+        const review = await dao.findReviewById(userId);
+        res.json(review);
+    }
+
+    app.get("/api/reviews", findAllReviews);
+    app.post("/api/reviews", createReview);
+    app.get("/api/reviews/:reviewId", findReviewById);
+    app.put("/api/reviews/:reviewId", editReview);
+    app.delete("/api/reviews/:reviewId", deleteReview);
+    app.get("/api/reviews/drinks/:idDrink", findReviewsForDrink);
+    app.get("/api/reviews/users/:userId", findReviewsFromUser);
+}
+
+export default ReviewsRoutes;
