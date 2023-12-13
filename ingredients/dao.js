595a2921347e5eee3f38ec7ea950f97bd21070a4
Jack Rifkin <jack.rifkin@icloud.com>
2023-12-13 15:53:50 -0500

    fix findMixrIngredient routes + dao functions

--- ingredients/dao.js
+++ ingredients/dao.js
@@ -4,14 +4,17 @@ export const createIngredient = (ingredient) => model.create(ingredient);
 
 export const findAllIngredients = () => model.find();
 
-export const findIngredientById = (ingredientId) => model.findById(ingredientId);
+export const findIngredientById = (ingredientId) => model.findOne({ idIngredient: ingredientId });
 
-export const findIngredientByName = (ingredientName) =>
-    model.findOne({ strIngredient: ingredientName });
+export const findIngredientByName = (ingredientName) => {
+    const regex = new RegExp(ingredientName, 'i');
+    const ingredient = model.findOne({ strIngredient: regex });
+    return ingredient;
+}
 
 export const findTop5IngredientsByPartialName = (partialName) => {
     // case insensitive regex for potential matches
-    const regex = new RegExp(`^${partialName}`, 'i'); 
+    const regex = new RegExp(`^${partialName}`, 'i');
     const top5Ingredients = model.find({ strIngredient: regex }).limit(5);
     return top5Ingredients;
 }
